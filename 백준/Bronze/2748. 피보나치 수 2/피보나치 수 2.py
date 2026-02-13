435ffe6be613889592076e0af962b06a95b187cc
효오 <spec3290@gmail.com>
2026-02-13 10:12:08 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    fibarr = [0,1]
+
+    for i in range(2, n+1):
+        fibarr.append(fibarr[i-1] + fibarr[i-2])
+    print(fibarr[n])
